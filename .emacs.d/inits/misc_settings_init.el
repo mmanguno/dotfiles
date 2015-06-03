37965e077eb20896f2da4b8b6cb220ee34853adc
Mitchell Manguno <mmanguno3@gatech.edu>
2015-06-03 14:52:43 -0500

    Adds ability to open new files in same frame
    http://superuser.com/questions/277755/emacs-opens-files-in-a-new-frame-when-opened-with-open-a

--- .emacs.d/inits/misc_settings_init.el
+++ .emacs.d/inits/misc_settings_init.el
@@ -1,2 +1,4 @@
 ;; Set the default directory
 (setq default-directory "/Users/mitchell")
+;; Open up new files in the same frame (i.e. window)                             
+(setq ns-pop-up-frames nil)
